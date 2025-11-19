c4c3cd2354de4e1e69822210d11e15d891da1cc2
Yuven91 <evenofstad@gmail.com>
2025-11-19 12:53:23 +0100

    cleaned the code

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
-pygame.init()
+
 
 
 
 def main():
+    pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while 1 != 0:
-      for event in pygame.event.get():
-           if event.type == pygame.QUIT:
-             return
-    log_state()
-    screen.fill("black")
-    pygame.display.flip()
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        pygame.display.flip()
 
 
 
