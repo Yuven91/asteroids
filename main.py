3d662c080e67495f22495600112bbef46ad4f77e
Yuven91 <evenofstad@gmail.com>
2025-11-20 10:26:59 +0100

    cleanet dt

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -20,6 +25,8 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        dt = (clock.tick(60) / 1000)
+        print(dt)
 
 
 
