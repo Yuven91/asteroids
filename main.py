274d9b093bf30366b91c118fbea2ed6430405beb
Yuven91 <evenofstad@gmail.com>
2025-11-21 13:38:47 +0100

    weird import error

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
-
-
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
 
 
 
 def main():
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     pygame.init()
+
+    
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -18,18 +29,31 @@ def main():
     dt = 0
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
-        log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         
         screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
         dt = (clock.tick(60) / 1000)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+
+        pygame.display.flip()
+        log_state()
+        
 
 
 
